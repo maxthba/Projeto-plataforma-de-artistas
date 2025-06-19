107d85e3ce1f59f057c8de8770d93de22d6d8ac6
maxthba <maxthomaz2006@gmail.com>
2025-06-19 12:22:58 -0300

    0.2

--- projeto-1.py
+++ projeto-1.py
@@ -1,3 +1,28 @@
 artistas_cadas = {}
-def
-
+def cadastro_artista():
+    nome= input("digite seu nome completo:")
+    prof = input("digite seu ramo na arte:")
+    est= input("digite seu estilo artistico:")
+    cid= input("digite sua cidade:")
+    cpf = int(input("digite seu cpf:"))
+    while cpf < 10000000000:
+        print("cpf invalido")
+        cpf=int(input("digite outro cpf:"))
+    while cpf in artistas_cadas:
+        print("ja possui cadastro na plataforma")
+        cpf=int(input("digite outro cpf:"))
+    nome = str(nome).lower()
+    prof = str(prof).lower()
+    est = str(est).lower()
+    cid = str(cid).lower()
+    dados_artista = {"nome": nome, "profissão":prof, "estilo":est,"cidade":cid}
+    artistas_cadas[cpf] = dados_artista
+def pesquisa()
+    Print(///////////////////////////////////)
+    Print(//  Digite como quer pesquisar   //)
+    Print(//             nome              //)
+    Print(//           profissão           //)
+    Print(//            estilo             //)
+    Print(///////////////////////////////////)
+    pesquisa = input()
+    if 
