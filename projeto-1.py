94d6258fd8b1fd81c7867f3e403587aab668ae9c
maxthba <maxthomaz2006@gmail.com>
2025-06-19 16:15:41 -0300

    0.3

--- projeto-1.py
+++ projeto-1.py
@@ -1,28 +1,95 @@
+import os
 artistas_cadas = {}
+
+def limp_term():
+    if os.name == "nt":
+        _= os.system("cls")
+    else:
+        _= os.system("clear")
+
 def cadastro_artista():
-    nome= input("digite seu nome completo:")
-    prof = input("digite seu ramo na arte:")
-    est= input("digite seu estilo artistico:")
-    cid= input("digite sua cidade:")
-    cpf = int(input("digite seu cpf:"))
+    nome= input("digite seu nome completo: ")
+    limp_term()
+    prof = input("digite seu ramo na arte: ")
+    limp_term()
+    est= input("digite seu estilo artistico: ")
+    limp_term()
+    cid= input("digite sua cidade: ")
+    limp_term()
+    cpf = int(input("digite seu cpf: "))
+    limp_term()
     while cpf < 10000000000:
         print("cpf invalido")
-        cpf=int(input("digite outro cpf:"))
+        cpf=int(input("digite outro cpf: "))
+        limp_term()
     while cpf in artistas_cadas:
         print("ja possui cadastro na plataforma")
-        cpf=int(input("digite outro cpf:"))
+        cpf=int(input("digite outro cpf: "))
+        limp_term()
     nome = str(nome).lower()
     prof = str(prof).lower()
     est = str(est).lower()
     cid = str(cid).lower()
     dados_artista = {"nome": nome, "profissão":prof, "estilo":est,"cidade":cid}
     artistas_cadas[cpf] = dados_artista
-def pesquisa()
-    Print(///////////////////////////////////)
-    Print(//  Digite como quer pesquisar   //)
-    Print(//             nome              //)
-    Print(//           profissão           //)
-    Print(//            estilo             //)
-    Print(///////////////////////////////////)
+
+def pesquisa():
+    print("///////////////////////////////////")
+    print("//     Digite como pesquisar     //")
+    print("//             nome              //")
+    print("//           profissão           //")
+    print("//            estilo             //")
+    print("//            cidade             //")
+    print("///////////////////////////////////")
     pesquisa = input()
-    if 
+    limp_term()
+    encontrados = {}
+
+    if pesquisa == "nome":
+        nome_busca = input("digite o nome: ")
+        nome_busca = nome_busca.lower()
+        limp_term()
+        for cpf, dados_artista in artistas_cadas.items():
+            if dados_artista["nome"] == nome_busca:
+                print(f"cpf:{cpf}")
+                encontrados[cpf] = dados_artista
+                for chave, valor in dados_artista.items():
+                    print(f"{chave.capitalize()}: {valor}")
+
+    elif pesquisa == "profissão":
+        nome_busca = input("digite a modalidade: ")
+        nome_busca = nome_busca.lower()
+        limp_term()
+        for cpf, dados_artista in artistas_cadas.items():
+            if dados_artista["profissão"] == nome_busca:
+                print(f"cpf:{cpf}")
+                encontrados[cpf] = dados_artista
+                for chave, valor in dados_artista.items():
+                    print(f"{chave.capitalize()}: {valor}")
+    
+    elif pesquisa == "estilo":
+        nome_busca = input("digite o estilo de arte: ")
+        nome_busca = nome_busca.lower()
+        limp_term()
+        for cpf, dados_artista in artistas_cadas.items():
+            if dados_artista["estilo"] == nome_busca:
+                print(f"cpf:{cpf}")
+                encontrados[cpf] = dados_artista
+                for chave, valor in dados_artista.items():
+                    print(f"{chave.capitalize()}: {valor}")
+    
+    elif pesquisa == "cidade":
+        nome_busca = input("digite a cidade: ")
+        nome_busca = nome_busca.lower()
+        limp_term()
+        for cpf, dados_artista in artistas_cadas.items():
+            if dados_artista["cidade"] == nome_busca:
+                print(f"cpf:{cpf}")
+                encontrados[cpf] = dados_artista
+                for chave, valor in dados_artista.items():
+                    print(f"{chave.capitalize()}: {valor}")
+    else:
+        limp_term()
+        pesquisa = input("digite uma opção valida: ")
+pesquisa()
+
